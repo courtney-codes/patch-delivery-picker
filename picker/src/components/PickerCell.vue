69721573ff1e3c7766811a5ab13ea46fe161d923
courtney-codes <courtney.senior@medicspot.co.uk>
2020-12-14 08:57:23 +0000

    feat: simple picker cell component

---
+++ picker/src/components/PickerCell.vue
@@ -0,0 +1,34 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: {}
+    },
+    selected: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    selectTimeslot() {
+      this.$emit("timeslotSelected", value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.picker-cell {
+  cursor: pointer;
+  
+  &--disabled {
+    pointer-events: none;
+    cursor: not-allowed;
+  }
+}
+</style>
