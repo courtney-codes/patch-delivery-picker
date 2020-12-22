e3998886b089d7b97d773b92f5fd3de8748109af
Courtney Senior <courtney.senior@live.com>
2020-12-22 01:02:29 +0000

    feat: fetch and iterate through delivery slots to render columns

--- picker/src/components/Picker.vue
+++ picker/src/components/Picker.vue
@@ -1,31 +1,52 @@
 <template>
-  <div>
+  <div class="picker-container">
     <h1>
-      Currently selected delivery slot: {{ selectedTimeslot.date }}, {{ selectedTimeslot.timeslot }}
+      Currently selected delivery slot: {{ selectedTimeslot.date }} {{ selectedTimeslot.timeslot }}
     </h1>
-    <PickerColumn
-      :value="date"
-      :timeslots="timeslotOptions"
-      @dateTimeslotSelected="selectTimeslot"
-    />
+    <div class="picker-grid">
+      <PickerColumn
+        v-for="(deliverySlot, index) in deliverySlots"
+        :key="index"
+        :deliverySlot="deliverySlot"
+        :timeslots="timeslotOptions"
+        @dateTimeslotSelected="selectTimeslot"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import PickerColumn from '@/components/PickerColumn.vue';
-import { DateTime } from 'luxon';
+import axios from 'axios';
+import { AVAILABLE_DELIVERY_SLOTS } from '@/util/constants';
 
 export default {
+  mounted() {
+    this.fetchDeliverySlots();
+  },
   data() {
     return {
-      date: DateTime.local().toISODate(),
+      deliverySlots: [],
       selectedTimeslot: {},
-      timeslotOptions: ['AM', 'PM', 'EVE'],
+      timeslotOptions: AVAILABLE_DELIVERY_SLOTS,
     };
+  },
+  computed: {
+    testSlots() {
+      return Object.values(this.deliverySlots[2].slots).map((slot) => slot.booked);
+    },
   },
   methods: {
     selectTimeslot($event) {
       this.selectedTimeslot = $event;
+    },
+    async fetchDeliverySlots() {
+      try {
+        const { data } = await axios.get('http://localhost:3000/delivery-slots');
+        this.deliverySlots = data;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
   components: {
@@ -34,4 +55,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.picker-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-flow: column;
+  grid-gap: 16px;
+}
+</style>
